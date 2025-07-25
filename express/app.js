8ff9ffe91ea027b7e4bb29ee856860e40b8749ae
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-26 00:23:01 +0530

    serving static file

--- express/app.js
+++ express/app.js
@@ -1,11 +1,22 @@
 import express from 'express';
 import {PORT} from "./env.js";
+import path from 'path';
 //const PORT=process.env.PORT ||3000;
 const app =express();
+// const __filename=new URL(import.meta.url).pathname;
+// console.log(__filename);
 
-app.get("/",(req,res)=>{
-    res.send(" Hello world");
-});
+// serving static file 
+ const staticPath=path.join(import.meta.dirname ,"public");
+app.use(express.static(staticPath));
+
+
+// app.get("/",(req,res)=>{
+    
+//     const homePagePath=path.join(import.meta.dirname ,"public","index.html");
+//     res.sendFile(homePagePath);
+
+// });
 app.listen(PORT,()=>{
     console.log(`server is running at port: ${PORT}`);
 });
