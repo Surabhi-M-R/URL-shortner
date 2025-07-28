9a52a9240c7ded55bc3fedcf1d5827d1b068234e
Surabhi-M-R <surabhimr27@gmail.com>
2025-07-28 18:13:02 +0530

    crud operation

--- mongodb/mongodb.driver.js
+++ mongodb/mongodb.driver.js
@@ -4,4 +4,31 @@ await client.connect();
 
 const db=client.db("node_js");
 const userCollection=db.collection("user");
-userCollection.insertOne({names:"sush", age:"21"});
+//userCollection.insertOne({names:"sush", age:"21"});
+
+userCollection.insertMany([{names:"sura",age: 100},
+    {name:"verru",age:21},
+    {names:"harshitha",age:19}
+]);
+
+// Read
+
+ //const userCursor=userCollection.find();
+// console.log(userCursor);
+
+// for await (const user of userCursor){
+//     console.log(user)
+// };
+
+// const userCursor= await userCollection.find().toArray();
+// console.log(userCursor);
+
+const user=await userCollection.findOne({names:"sush"});
+//console.log(user);
+console.log(user._id);
+
+// update
+await userCollection.updateOne({names:"sush"},{$set:{age:21}});
+
+await userCollection.deleteOne({names:"sush"});
+cd
